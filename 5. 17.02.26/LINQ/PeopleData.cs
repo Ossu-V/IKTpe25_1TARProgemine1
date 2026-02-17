ad89c0baed9522df26912ab26108f842e0c195a0
Ossu-V <oscarvalk7@gmail.com>
2026-02-17 15:53:57 +0200

    PeopleListLINQ

--- 5. 17.02.26/LINQ/PeopleData.cs
+++ 5. 17.02.26/LINQ/PeopleData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LINQ.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,59 @@ using System.Threading.Tasks;
 
 namespace LINQ
 {
-    class PeopleData
+    public class PeopleData
     {
+        public static readonly List<PeopleList> peoples = new List<PeopleList>
+        {
+            new PeopleList()
+            {
+                Id = 1,
+                Name = "Moona",
+                Age = 31,
+                GenderId = Guid.Parse("9a314d3b-896e-487f-80ca-9db39ac1e923")
+            },
+            new PeopleList()
+            {
+                Id = 2,
+                Name = "Poona",
+                Age = 21,
+                GenderId = Guid.Parse("7d37e4eb-a136-4e02-8c6d-f5f899b80dad")
+            },
+            new PeopleList()
+            {
+                Id = 3,
+                Name = "Ron",
+                Age = 18,
+                GenderId = Guid.Parse("7d37e4eb-a136-4e02-8c6d-f5f899b80dad")
+            },
+            new PeopleList()
+            {
+                Id = 4,
+                Name = "Anna",
+                Age = 20,
+                GenderId = Guid.Parse("9a314d3b-896e-487f-80ca-9db39ac1e923")
+            },
+            new PeopleList()
+            {
+                Id = 5,
+                Name = "Mari",
+                Age = 19,
+                GenderId = Guid.Parse("9a314d3b-896e-487f-80ca-9db39ac1e923")
+            },
+            new PeopleList()
+            {
+                Id = 6,
+                Name = "Mari",
+                Age = 21,
+                GenderId = Guid.Parse("9a314d3b-896e-487f-80ca-9db39ac1e923")
+            },
+            new PeopleList()
+            {
+                Id = 7,
+                Name = "Bill",
+                Age = 21,
+                GenderId = Guid.Parse("7d37e4eb-a136-4e02-8c6d-f5f899b80dad")
+            },
+        };
     }
 }
--- 5. 17.02.26/LINQ/Program.cs
+++ 5. 17.02.26/LINQ/Program.cs
@@ -1,10 +1,63 @@
-﻿namespace LINQ
+﻿using LINQ.Models;
+using System;
+
+namespace LINQ
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Kutsume esile LINQ läbi switchi");
+            Console.WriteLine("Vali vastav link numbriga");
+            Console.WriteLine("1. Where");
+            Console.WriteLine("2. Where ja otsib nime järgi");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    WhereLINQ();
+                    break;
+
+                case 2:
+                    WhereByNameLINQ();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        //teeme uue meetoid
+        public static void WhereLINQ()
+        {
+            var peopleAge = PeopleData.peoples
+                .Where(x => x.Age > 20 && x.Age < 23);
+
+            //kasuta muutujat peaopleAge ja kuvanda andmed esile
+            //kasuta foreachi
+
+          foreach (var person in peopleAge)
+            {
+                Console.WriteLine(person.Name);
+            }
+        }
+
+        public static void WhereByNameLINQ()
+        {
+            Console.WriteLine("Kirjuta inimese nimi: ");
+            string name = Console.ReadLine();
+
+            //kasutada where inimese otsimiseks
+            //otsimine toimub nime alusel
+
+            var peopleName = PeopleData.peoples
+                .Where(x => x.Name == name);
+
+            foreach (var people in peopleName)
+            {
+                Console.WriteLine(people.Name + " " + people.Age);
+            }
         }
     }
 }
