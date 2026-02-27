1a1d4ca12198886711c14715d1d3d8d11d06dea2
Ossu-V <oscarvalk7@gmail.com>
2026-02-27 10:07:02 +0200

    LinqTakeSkip

---
+++ 7. 27.02.26/LinqTakeSkip/PeopleList.cs
@@ -0,0 +1,58 @@
+﻿namespace LinqTakeSkip
+{
+    public class PeopleList
+    {
+        public static readonly List<People> people = new List<People>
+        {
+            new People()
+            {
+                Id = 1,
+                Name = "Moona",
+                Age = 21,
+                GenderId = Guid.Parse("9a314d3b-896e-487f-80ca-9db39ac1e923")
+            },
+            new People()
+            {
+                Id = 2,
+                Name = "Joonas",
+                Age = 20,
+                GenderId = Guid.Parse("7d37e4eb-a136-4e02-8c6d-f5f899b80dad")
+            },
+            new People()
+            {
+                Id = 3,
+                Name = "Ron",
+                Age = 19,
+                GenderId = Guid.Parse("7d37e4eb-a136-4e02-8c6d-f5f899b80dad")
+            },
+            new People()
+            {
+                Id = 4,
+                Name = "Anna",
+                Age = 22,
+                GenderId = Guid.Parse("9a314d3b-896e-487f-80ca-9db39ac1e923")
+            },
+            new People()
+            {
+                Id = 5,
+                Name = "Mari",
+                Age = 19,
+                GenderId = Guid.Parse("9a314d3b-896e-487f-80ca-9db39ac1e923")
+            },
+            new People()
+            {
+                Id = 6,
+                Name = "Bill",
+                Age = 15,
+                GenderId = Guid.Parse("9a314d3b-896e-487f-80ca-9db39ac1e923")
+            },
+            new People()
+            {
+                Id = 7,
+                Name = "Jumbo",
+                Age = 21,
+                GenderId = Guid.Parse("7d37e4eb-a136-4e02-8c6d-f5f899b80dad")
+            },
+        };
+    }
+}
---
+++ 7. 27.02.26/LinqTakeSkip/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace LinqTakeSkip
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kutsume esile LINQ meetodid");
+            Console.WriteLine("1. Skip");
+            Console.WriteLine("2. SkipWhile");
+            Console.WriteLine("3. TakeWhile");
+
+            //siin kasutada switchi ja peab saama Skip meetodit esile kutsuda
+
+            int choice = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            switch (choice)
+            {
+                case 1:
+                    Skip();
+                    break;
+
+                case 2:
+                    SkipWhile();
+                    break;
+
+                case 3:
+                    TakeWhile();
+                    break;
+
+                default:
+                    Console.WriteLine("Vale valik");
+                    break;
+            }
+        }
+        public static void Skip()
+        {
+            Console.WriteLine("---------Skip---------");
+            //kasuta skip ja jäta kolm tükki vahele
+
+            var skip = PeopleList.people.Skip(3);
+
+            foreach (var item in skip)
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.WriteLine();
+        }
+
+        //teete uue meetodi, aga kasutate SkipWhile ja vanemad, kui 18 aastat peab olema tingimus
+        public static void SkipWhile()
+        {
+            Console.WriteLine("---------SkipWhile---------");
+            //mis tähendab: => . See tähendab lambda märki ja selle
+            //abil saab kasutada pikema classi nimetuse asemel lühendit
+            //koos sees oleva muutujaga, mis antud juhul on x.
+
+            var skipWhile = PeopleList.people.SkipWhile(x => x.Age > 18);
+
+            foreach (var item in skipWhile)
+            {
+                Console.WriteLine(item.Id + " " + item.Name + " " + item.Age);
+            }
+            Console.WriteLine();
+            //skipWhile jätab loendis nii kaua vahele ridu kuni vastab tingimusele
+            //ehk antud juhul jätab read vahele kuni leiab 18 a isiku ja
+            //peale seda hakkab infot jälle kuvama olemata vanuse tingimusest
+        }
+
+        //kasutada TakeWhile ja kutsuda see esile switchis
+        //tingimus on Age > 18
+
+        public static void TakeWhile()
+        {
+            Console.WriteLine("---------TakeWhile---------");
+
+            var takeWhile = PeopleList.people.TakeWhile(x => x.Age > 18);
+
+            foreach (var item in takeWhile)
+            {
+                Console.WriteLine(item.Id + " " + item.Name + " " + item.Age);
+            }
+            Console.WriteLine();
+        }
+    }
+}
