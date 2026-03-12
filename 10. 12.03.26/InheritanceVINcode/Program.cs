04e0142361c32b4b5f8c63b540014349f7755109
Opilane TTHK <oscarvalk7@gmail.com>
2026-03-12 11:31:12 +0200

    InheritanceVinCode

---
+++ 10. 12.03.26/InheritanceVINcode/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace InheritanceVINcode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta VIN kood: ");
+            //Teha pärilus
+            //on olemas class nimega Machine
+            //see pärib Cars classi
+            //Saab sisestada masina numbri
+            //konsool annab vastuse: Edukalt sisestatud
+            // VIN kood: VIN koodi nr
+
+            int vinCode = Convert.ToInt32(Console.ReadLine());
+
+            Machine machine = new Machine();
+            machine.SetVinCode(vinCode);
+
+            Console.WriteLine("Edukalt sisestatud");
+            Console.WriteLine("VIN kood: {0}", machine.GetVinCode());
+        }
+    }
+    class Car
+    {
+        public void SetVinCode(int vinCode)
+        {
+            vin = vinCode;
+        }
+        protected int vin;
+    }
+
+    class Machine : Car
+    {
+        public int GetVinCode()
+        {
+            return vin;
+        }
+    }
+}
