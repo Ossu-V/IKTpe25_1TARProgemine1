7c8d00435aa6507593f563e76358abb2945bb9cc
Opilane TTHK <oscarvalk7@gmail.com>
2026-03-19 10:16:23 +0200

    SortedAndTuple

---
+++ 12. 19.03.26/SortedAndTuple/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+
+namespace SortedAndTuple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sorted And Tuple");
+            Console.WriteLine("1. SortedList");
+            Console.WriteLine("2. Tuple");
+            Console.WriteLine("---------------------");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    SortedList();
+                    break;
+
+                case 2:
+                    Tuple();
+                    break;
+
+                default:
+                    Console.WriteLine("ERROR");
+                    break;
+            }
+            Console.WriteLine();
+        }
+
+        static void SortedList()
+        {
+            SortedList<int, string> sl = new SortedList<int, string>();
+
+            sl.Add(3, "Three");
+            sl.Add(1, "One");
+            sl.Add(2, "Two");
+
+            foreach (var item in sl)
+            {
+                Console.WriteLine($"Key: {item.Key}, Value: {item.Value}");
+            }
+        }
+
+        static void Tuple()
+        {
+            {
+                var tuple = (123, "Hello", true);
+
+                Console.WriteLine(tuple.Item1);
+                Console.WriteLine(tuple.Item2);
+                Console.WriteLine(tuple.Item3);
+            }
+        }
+    }
+}
