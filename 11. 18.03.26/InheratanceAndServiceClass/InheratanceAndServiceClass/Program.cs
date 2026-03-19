7d02a0b13e295441a8c14ac425b7b68d4d629268
Opilane TTHK <oscarvalk7@gmail.com>
2026-03-19 09:36:09 +0200

    InheritanceAndServiceClass

--- 11. 18.03.26/InheratanceAndServiceClass/InheratanceAndServiceClass/Program.cs
+++ 11. 18.03.26/InheratanceAndServiceClass/InheratanceAndServiceClass/Program.cs
@@ -1,16 +1,116 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Hosting;
+﻿using InheritanceAndServiceClass.AppServices.Services;
+using InheritanceAndServiceClass.Core.ServiceInterface;
+using Microsoft.AspNetCore.Mvc;
 
-namespace InheratanceAndServiceClass
+
+namespace InheritanceAndServiceClass
 {
     internal class Program
     {
+        private readonly ICarServices _carServices;
+
+        public Program
+            (
+                ICarServices carServices
+            )
+        {
+            _carServices = carServices;
+        }
+
         static void Main(string[] args)
         {
-            var builder = Microsoft.AspNetCore.Builder
-                .WebApplication.CreateBuilder(args);
+            var builder = WebApplication.CreateBuilder(args);
 
-            Console.WriteLine("Hello, World!");
+            builder.Services.AddScoped<ICarServices, CarServices>();
+
+            Console.WriteLine("Hello, World Switch!");
+            Console.WriteLine("1. GetAsync");
+            Console.WriteLine("2. SaveAsync");
+            Console.WriteLine("3. UpdateData");
+            Console.WriteLine("4. EraseData");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    var app = builder.Build();
+                    using (var scope = app.Services.CreateScope())
+                    {
+                        var carServices = scope.ServiceProvider.GetRequiredService<ICarServices>();
+                        var program = new Program(carServices);
+                        program.GetAsync();
+                    }
+                    break;
+
+                case 2:
+                    app = builder.Build();
+                    using (var scope = app.Services.CreateScope())
+                    {
+                        var carServices = scope.ServiceProvider.GetRequiredService<ICarServices>();
+                        var program = new Program(carServices);
+                        program.SaveAsync();
+                    }
+                        break;
+
+                case 3:
+                    app = builder.Build();
+                    using (var scope = app.Services.CreateScope())
+                    {
+                        var carServices = scope.ServiceProvider.GetRequiredService<ICarServices>();
+                        var program = new Program(carServices);
+                        program.UpdateData();
+                    }
+                    break;
+
+                case 4:
+                    app = builder.Build();
+                    using (var scope = app.Services.CreateScope())
+                    {
+                        var carServices = scope.ServiceProvider.GetRequiredService<ICarServices>();
+                        var program = new Program(carServices);
+                        program.EraseData();
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Error");
+                    break;
+            }
+            Console.Clear();
+        }
+
+        public IActionResult GetAsync()
+        {
+            _carServices.GetData();
+
+            return View();
+        }
+
+        private IActionResult View()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult SaveAsync()
+        {
+            _carServices.PostData();
+
+            return View();
+        }
+
+        public IActionResult UpdateData()
+        {
+            _carServices.PutData();
+
+            return View();
+        }
+
+        public IActionResult EraseData()
+        {
+            _carServices.PutData();
+
+            return View();
         }
     }
 }
--- 11. 18.03.26/InheratanceAndServiceClass/InheritanceAndServiceClass.Core/ServiceInterface/ICarServices.cs
+++ 11. 18.03.26/InheratanceAndServiceClass/InheritanceAndServiceClass.Core/ServiceInterface/ICarServices.cs
@@ -3,5 +3,20 @@
     public interface ICarServices
     {
         void GetData();
+
+        void PostData()
+        {
+            Console.WriteLine("Andmed on edukalt salvestatud");
+        }
+
+        void PutData()
+        {
+            Console.WriteLine("Andmed on edukalt uuendatud");
+        }
+
+        void DeleteData()
+        {
+            Console.WriteLine("Andmed on edukalt kustutatud");
+        }
     }
 }
