28770aff42e1fadad9fceb940b10fe881dd4bd3d
Opilane TTHK <230301839+Ossu-V@users.noreply.github.com>
2026-03-25 11:35:49 +0200

    Inheritance2

---
+++ 13. 25.03.26/Inheritance2/Lake.cs
@@ -0,0 +1,12 @@
+﻿namespace Inheritance2
+{
+    class Lake : Water
+    {
+        //tehke sama asi, mis River-ga ja kutsuge see Program classi Main meetodis esile
+
+        public override void DoSomething()
+        {
+            Console.WriteLine("Lake method, and it has " + Flow + " and " + Length + " is in meters");
+        }
+    }
+}
---
+++ 13. 25.03.26/Inheritance2/River.cs
@@ -0,0 +1,14 @@
+﻿namespace Inheritance2
+{
+    //River on antud juhul alamklass ja viitab Waterile ehk peamisele classile
+     class River : Water
+     {
+        //kui kirjutan override, siis kirjutab Water meetodis oleva DoSomething meetodi üle
+        //kui panen siia public virtual void, siis ei kirjuta Water meetodi DoSomething-t üle
+        public override void DoSomething()
+        {
+            //Water classis on olemas muutujad Flow ja Lenght ja sellepärast ei pea neid siin uuesti defineerima
+            Console.WriteLine("River method, and it has " + Flow + " and " + Length + " is in meters");
+        }
+     }
+}
