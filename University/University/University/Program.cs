9f34ffcfab07bc128361951530d7371b7490d07e
Opilane TTHK <oscarvalk7@gmail.com>
2026-04-14 13:42:24 +0300

    DbInitializer

--- University/University/University/Program.cs
+++ University/University/University/Program.cs
@@ -12,6 +12,10 @@ namespace University
             builder.Services.AddDbContext<UniversityContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("UniversityContext")));
 
+            //Add database exception filter for development environment 
+            //This will show detailed database errors durning development
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
