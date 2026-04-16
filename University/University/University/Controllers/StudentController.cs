e41e0f72d4051c8e97993014f496b54a99ea242e
Opilane TTHK <oscarvalk7@gmail.com>
2026-04-16 14:51:12 +0300

    ViewModel

--- University/University/University/Controllers/StudentController.cs
+++ University/University/University/Controllers/StudentController.cs
@@ -17,9 +17,43 @@ namespace University.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            var data = await _context.Students.ToListAsync();
+            //leiame kõik student´id ja teisendame need StudentIndexViewModel´iks
+            //miks peab kasutama await?
+            //kui me kasutame await, siis me ootame kuni päring on lõpetatud
+            //ja saame tulemuse, enne kui me jätkame koodi täitmist
+            var result = await _context.Students.Select(s => new ViewModel.StudentIndexViewModel
+            {
+                Id = s.Id,
+                LastName = s.LastName,
+                FirstMidName = s.FirstMidName,
+                EnrollmentDate = s.EnrollmentDate
+                //miks kasutame ToListAsync()?
+                //kui kasutame ToListAsync(), siis me saame tulemuse listina
+            }).ToListAsync();
 
-            return View(data);
+            return View(result);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            //kui id on null, siis tagastame NotFound() tulemuse
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            //leiame student´i id järgi
+            var student = await _context.Students
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            //kui student on null, siis tagastame NotFound() tulemuse
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            //kui student on leitud, siis tagastame View(student) tulemuse
+            return View(student);
         }
     }
 }
