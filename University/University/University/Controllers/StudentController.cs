82d390de3458bc44a77f42d541733615c87a4b0e
Opilane TTHK <oscarvalk7@gmail.com>
2026-04-28 14:09:49 +0300

    studentUpdate/DeleteViewModel

--- University/University/University/Controllers/StudentController.cs
+++ University/University/University/Controllers/StudentController.cs
@@ -152,5 +152,77 @@ namespace University.Controllers
 
             return View(vm);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(StudentUpdateViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var student = new Models.Student
+                {
+                    Id = vm.Id,
+                    LastName = vm.LastName,
+                    FirstMidName = vm.FirstMidName,
+                    EnrollmentDate = vm.EnrollmentDate
+                };
+
+                var studentUpdate = student.Id;
+                //lisame stundent´i andmebaasi ja salvestame muudatused
+                _context.Update(student);
+                //miks kasutame await?
+                //kui me kasutame await, siis me ootame kuni salvestamine on lõpetatud
+                await _context.SaveChangesAsync();
+                //pärast salvestamist suuname kasutaja tagasi Index vaatesse
+
+                //kui andmed on uuendatud, siis suunab tagasi Update vaatesse, kus saab kohe uuesti andmeid uuendada.
+                //Hetkel suunab Indexi vaatesse peale uuendust
+                return RedirectToAction(nameof(Update), new {id = studentUpdate});
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        //tehke Delete Get meetod koos vaatega
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students
+                .Include(s => s.Enrollments)
+                    .ThenInclude(e => e.Course)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            var vm = new StudentDeleteViewModel
+            {
+                Id = student.Id,
+                LastName = student.LastName,
+                FirstMidName = student.FirstMidName,
+                EnrollmentDate = student.EnrollmentDate,
+                EnrollmentsVm = (student.Enrollments ?? Enumerable.Empty<Enrollment>())
+                    .Select(x => new EnrollmentViewModel
+                    {
+                        CourseId = x.CourseId,
+                        Grade = x.Grade,
+                        CourseVm = new CourseViewModel
+                        {
+                            CourseId = x.Course?.CourseId ?? 0,
+                            Title = x.Course?.Title,
+                            Credits = x.Course?.Credits ?? 0
+                        }
+                    }).ToArray()
+            };
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(vm);
+        }
     }
 }
