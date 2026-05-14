ab3c3ae70d84acf4f902f513cb92307898f4d8c7
Opilane TTHK <oscarvalk7@gmail.com>
2026-05-14 14:55:49 +0300

    Classes/CourseAssignment/Models

---
+++ University/University/University/Models/Instructor.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace University.Models
+{
+    public class Instructor
+    {
+        public int Id { get; set; }
+
+        public string LastName { get; set; }
+
+        [Column("FirstName")]
+        public string FirstMidName { get; set; }
+
+        public DateTime EnrollmentDate { get; set; }
+
+        //mis on ICollection?
+        //Nimekiri kuhu saab panna mitut objekti.
+
+        public ICollection<CourseAssignment> CourseAssignments { get; set; }
+
+        //miks siin ei kasutata ICollection, vaid lihtsalt OfficeAssignment?
+        //siin EI kasutata ICollection, sest seos on ainult üks ühele seos Instructoriga.
+        public OfficeAssignment OfficeAssignments { get; set; }
+    }
+}
