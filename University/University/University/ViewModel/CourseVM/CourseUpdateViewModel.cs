8b2de3072117b73fa437e3cabd2ff3d5c303b71c
Opilane TTHK <oscarvalk7@gmail.com>
2026-05-28 12:07:14 +0300

    CourseUpdateViewModelFix

--- University/University/University/ViewModel/CourseVM/CourseUpdateViewModel.cs
+++ University/University/University/ViewModel/CourseVM/CourseUpdateViewModel.cs
@@ -5,11 +5,13 @@ namespace University.ViewModel.CourseVM
 {
     public class CourseUpdateViewModel
     {
-            public int CourseId { get; set; }
-            public string Title { get; set; }
-            public int Credits { get; set; }
-            public int DepartmentId { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Number")]
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public int DepartmentId { get; set; }
 
-            public CourseDepartmentIndexViewModel Department { get; set; }
+        public CourseDepartmentIndexViewModel Department { get; set; }
     }
 }
